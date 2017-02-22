d26b3e1625aeefa579e5bb523ace994053110d9c
Arpitha <prasanna@tcd.ie>
2017-02-22 20:12:03 +0000

    adding Profile activity and database connection to the Mobile App.

---
+++ app/src/main/java/com/smarket/shopit/UserInformation.java
@@ -0,0 +1,18 @@
+package com.smarket.shopit;
+
+/**
+ * Created by Arpitha on 2/19/2017.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+    public String dob;
+
+    public UserInformation(String name, String address, String dob){
+        this.name = name;
+        this.address = address;
+        this.dob = dob;
+    }
+}
---
+++ app/src/main/java/com/smarket/shopit/profileActivity.java
@@ -0,0 +1,91 @@
+package com.smarket.shopit;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class profileActivity extends AppCompatActivity implements TextView.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
+    private DatabaseReference databaseReference;
+
+    private EditText editTextName, editTextAddress, editTextDOB;
+    private Button buttonSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextDOB = (EditText) findViewById(R.id.editTextDOB);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+
+        textViewUserEmail.setText("Welcome "+user.getDisplayName());
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        buttonLogout.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+
+    }
+
+    private void saveUserInformation(){
+        String name = editTextName.getText().toString().trim();
+        String add = editTextAddress.getText().toString().trim();
+        String dob = editTextDOB.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name, add, dob);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        Toast.makeText(this, "Information saved...", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onClick(View view) {
+        //if logout is pressed
+        if(view == buttonLogout){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        if(view == buttonSave){
+            saveUserInformation();
+        }
+    }
+}
